aa382dae03f58789eeca771393e316ade4ccd171
Cristian-M-B <crisbaronetto@hotmail.com>
2021-12-14 19:05:00 -0300

    changes

--- client/src/components/navBar/NavBar.js
+++ client/src/components/navBar/NavBar.js
@@ -1,17 +1,15 @@
 import { Link } from 'react-router-dom';
 import SearchBar from '../searchBar/SearchBar.js';
-import './NavBar.css';
+import styled from './NavBar.module.css';
 
 export default function NavBar() {
-    return <div className='navContainer'>
-        <div>
+    return <div className={styled.container}>
             <Link to='/home/recipes'>
-                <button>Home</button>
+                <button className={styled.btn}>Home</button>
             </Link>
-            <Link to='/home/create'>
-                <button>Create Recipe</button>
-            </Link>
-        </div>
             <SearchBar />
+            <Link to='/home/create'>
+                <button className={styled.btn}>New</button>
+            </Link>
     </div>
 }
