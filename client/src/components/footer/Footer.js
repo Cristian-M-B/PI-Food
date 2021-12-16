32a210710f21911340085c226292c0800312dbee
Cristian-M-B <crisbaronetto@hotmail.com>
2021-12-16 16:06:05 -0300

    component footer

---
+++ client/src/components/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BsLinkedin } from 'react-icons/bs'
+import styled from './Footer.module.css';
+
+export default function Footer() {
+    return (
+        <footer className={styled.container}>
+            <p>Cristian Baronetto</p>
+            <a href='https://www.linkedin.com/in/cristian-baronetto' target='_blank'><BsLinkedin /></a>
+        </footer>
+    )
+}
