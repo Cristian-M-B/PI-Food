0b61b6670eefe9d36ee72ce5a68638eee38442c8
Cristian-M-B <crisbaronetto@hotmail.com>
2022-02-01 16:20:49 -0300

    update footer

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { BsLinkedin } from 'react-icons/bs';
+import { BsLinkedin, BsFillBriefcaseFill, BsFileEarmarkPdf } from 'react-icons/bs';
 import styled from './Footer.module.css';
 
 export default function Footer() {
     return (
         <footer className={styled.container}>
             <p>Cristian Baronetto</p>
-            <a href='https://www.linkedin.com/in/cristian-baronetto' target='_blank' rel='noreferrer'><BsLinkedin /></a>
+            <div>
+                <a href='https://www.linkedin.com/in/cristian-baronetto' target='_blank' rel='noreferrer'><BsLinkedin /></a>
+                <a href='https://cristianbaronetto.vercel.app/' target='_blank' rel='noreferrer'><BsFillBriefcaseFill /></a>
+                <a href='https://drive.google.com/file/d/1RxU-PS31Dnl6xrYCbIyAXI9s51hlbREu/view' target='_blank' rel='noreferrer'><BsFileEarmarkPdf /></a>
+            </div>
         </footer>
     )
 }
