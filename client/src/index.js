953c1dd67f3ae3df4ab8d4d4e4bed100d6036c9b
Cristian-M-B <crisbaronetto@hotmail.com>
2022-12-28 13:51:35 -0300

    modification

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
-import {Provider} from 'react-redux'
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import store from './redux/store/index.js';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
